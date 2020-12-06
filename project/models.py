7accb822b5cf3fc7279591d78fc88c1bef08086a
said belhadj <saidbelhadj0312@gmail.com>
2020-12-06 22:19:51 +0100

    Added views and model to display list of project

--- project/models.py
+++ project/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+
--- project/views.py
+++ project/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from project.models import Project
+
+
+def index(request):
+    latest_project_list = Project.objects.order_by('-pub_date')[:3]
+    template = loader.get_template('project/index.html')
+    context = {
+        'latest_project_list': latest_project_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, project_id):
+    return HttpResponse("You're looking at project %s." % project_id)
